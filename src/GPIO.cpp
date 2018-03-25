ad5c559083aa8123be83c521935e7fb67561ba59
bnuh <bryan.melanson@gmail.com>
2018-03-25 19:43:02 -0230

    Initial commit

---
+++ src/GPIO.cpp
@@ -0,0 +1,83 @@
+/*
+Daniel Cook 2013
+daniel@daniel-cook.net
+*/
+
+#include "GPIO.h"
+
+namespace BeagleUtil
+{
+
+    GPIO::GPIO(std::string path){
+        _path = path;
+    }
+
+    void GPIO::exportPin(unsigned int pin){
+        std::ofstream fout;
+        fout.open(std::string(_path+"/export").c_str(), std::ios::out);
+
+        fout << pin;
+
+        fout.close();
+    }
+
+    void GPIO::unexportPin(unsigned int pin){
+        std::ofstream fout;
+        fout.open(std::string(_path+"/unexport").c_str(), std::ios::out);
+
+        fout << pin;
+
+        fout.close();
+    }
+
+    void GPIO::setPinDirection(unsigned int pin, std::string dir){
+        std::ofstream fout;
+        std::stringstream ss;
+        ss << _path+"/gpio" << pin << "/direction";
+        fout.open(ss.str().c_str(), std::ios::out);
+
+        fout << dir;
+
+        fout.close();
+    }
+
+    void GPIO::setPinValue(unsigned int pin, unsigned int value){
+        std::ofstream fout;
+        std::stringstream ss;
+        ss << _path+"/gpio" << pin << "/value";
+        fout.open(ss.str().c_str(), std::ios::out);
+
+        fout << value;
+
+        fout.close();
+    }
+
+    unsigned int GPIO::getPinValue(unsigned int pin){
+        std::ifstream fin;
+        std::stringstream ss;
+        ss << _path+"/gpio" << pin << "/value";
+        fin.open(ss.str().c_str(), std::ios::in);
+
+        unsigned int val;
+        fin >> val;
+
+        fin.close();
+
+        return val;
+    }
+
+    int gpio_omap_mux_setup(const char *omap_pin0_name, const char *mode){
+        int fd;
+        char buf[MAX_BUF];
+        snprintf(buf, sizeof(buf), SYSFS_OMAP_MUX_DIR "%s", omap_pin0_name);
+        fd = open(buf, O_WRONLY);
+        if (fd < 0) {
+            perror("failed to open OMAP_MUX");
+            return fd;
+        }
+        write(fd, mode, strlen(mode) + 1);
+        close(fd);
+        return 0;
+    }
+
+}
---
+++ src/PWM.cpp
@@ -0,0 +1,153 @@
+/*
+Daniel Cook 2013
+daniel@daniel-cook.net
+*/
+
+#include "PWM.h"
+
+namespace BeagleUtil
+{
+
+
+    PWM::PWM(std::string path){
+        std::ofstream fout1, fout2, fout3, fout4, fout5, fout6;
+
+        fout1.open("/sys/devices/platform/ocp/ocp\:P9_42_pinmux/state", std::ios::out);
+        fout2.open("/sys/devices/platform/ocp/ocp\:P9_14_pinmux/state", std::ios::out);
+
+        fout1 << 'pwm';
+        fout2 << 'pwm';
+        fout3 << 'gpio';
+        fout4 << 'gpio';
+        fout5 << 1;
+        fout6 << 1;
+
+        fout1.close();
+        fout2.close();
+        fout3.close();
+        fout4.close();
+        fout5.close();
+        fout6.close();
+
+        _path = path;
+        _dutyPercent = 0;
+    }
+
+    void PWM::run(){
+        std::ofstream fout;
+        fout.open(std::string(_path+"/enable").c_str(), std::ios::out);
+
+        fout << 1;
+
+        fout.close();
+
+        if(!_dutyPercent)
+            std::cout << "Warning: duty_percent = 0" << std::endl;
+    }
+
+    void PWM::stop(){
+        std::ofstream fout;
+        fout.open(std::string(_path+"/enable").c_str(), std::ios::out);
+
+        fout << 0;
+
+        fout.close();
+    }
+
+    bool PWM::request(){
+        if(isFree()){
+            std::ofstream fout;
+            fout.open(std::string(_path+"/request").c_str(), std::ios::out);
+
+            fout << 1;
+
+            fout.close();
+
+            return true;
+        }
+        else
+            return false;
+    }
+
+    bool PWM::release(){
+        if(!isFree()){
+            std::ofstream fout;
+            fout.open(std::string(_path+"/request").c_str(), std::ios::out);
+
+            fout << 0;
+
+            fout.close();
+
+            return true;
+        }
+        else
+            return false;
+    }
+
+    void PWM::setDutyPercent(float percent){
+        std::ofstream fout;
+        fout.open(std::string(_path+"/duty_percent").c_str(), std::ios::out);
+
+        fout << percent;
+        _dutyPercent = percent;
+
+        fout.close();
+    }
+
+    void PWM::setPeriodFreq(float freq){
+        setDutyPercent(0);
+
+        std::ofstream fout;
+        fout.open(std::string(_path+"/period_freq").c_str(), std::ios::out);
+
+        fout << freq;
+
+        fout.close();
+    }
+
+    void PWM::setPeriodFreqWithDutyPercent(float freq, float percent){
+        setPeriodFreq(freq);
+        setDutyPercent(percent);
+    }
+
+    float PWM::getDutyPercent(){
+        std::ifstream fin(std::string(_path+"/duty_percent").c_str(), std::ios::in);
+
+        float ret;
+        fin >> ret;
+
+        fin.close();
+        return ret;
+    }
+
+    float PWM::getPeriodFreq(){
+        std::ifstream fin(std::string(_path+"/period_freq").c_str(), std::ios::in);
+
+        float ret;
+        fin >> ret;
+
+        fin.close();
+        return ret;
+    }
+
+    bool PWM::isFree(){
+        std::ifstream fin;
+        fin.open(std::string(_path+"/request").c_str(), std::ios::in);
+
+        //std::string ret1, ret2, ret3;
+        //fin >> ret1 >> ret2 >> ret3;
+        char line[64];
+        fin.getline(line, 64);
+        std::string status = std::string(line);
+
+        fin.close();
+
+        //std::string status = ret1+ret2+ret3;
+
+        if(status.find("free") != std::string::npos)
+            return true;
+        else
+            return false;
+    }
+
+}
